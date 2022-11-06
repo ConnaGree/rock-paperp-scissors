4bde2fba23e7bec18fb0e21f424d42cf2f521df5
Zerubabel Yonas <93067387+ConnaGree@users.noreply.github.com>
2022-11-06 11:25:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
  
-let choiceName = ['Rock','Paper','Scissors'];
+let choiceName = ['Rock','Paper','Scissor'];
 function getComputerChoice(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length)
     const item = arr[randomIndex];
     return item;
 }
 
-
-let compWin = 0, useWin = 0;
 let computerSelection = getComputerChoice(choiceName);
 
+let compWin = 0, useWin = 0 , match = 0;
 
-let userSelection = prompt("What is your choice? ")
+
+
+
+// console.log(playRound(playerSelection,computerSelection));
+
+
+
+function game() {
+    
+    
+    for (let i = 0; i < 5; i++){
+        userSelection = prompt("What is your choice? ");
+        playerSelection = selection(userSelection);
+        console.log(`Match: ${++match}`)
+        console.log(`playerSelection: ${playerSelection}`)
+        console.log(`computerSelection: ${computerSelection}`)
+        console.log(playRound(playerSelection,computerSelection))
+        console.log(`playerScore: ${useWin}`, `computerScore: ${compWin}`)
+        
+        
+        computerSelection = getComputerChoice(choiceName)
+        
+       
+    }
+    
+
+
+}
+game();
+if (useWin < compWin){
+    console.log("***************** You Lose the Game! ************************");
+} else if (useWin > compWin){
+    console.log("***************** You Win the Game! ************************");
+}else {
+    console.log("***************** Tie! Play Again ************************")
+}
+
+
 function selection(select){
     let choice = select.charAt(0).toUpperCase() + select.slice(1).toLocaleLowerCase();
     return choice;
 }
 
-const playerSelection = selection(userSelection);
-// console.log(playerSelection, computerSelection)
+
 
 function playRound(playerSelection,computerSelection) {
-     if (playerSelection === "Rock" && computerSelection == "Paper"){
+     if (playerSelection == "Rock" && computerSelection == "Paper"){
         ++compWin;
         return 'You Lose! Paper beats Rock';
      }
-     else if (playerSelection === "Scissors" && computerSelection == "Paper") {
+     else if (playerSelection == "Scissor" && computerSelection == "Paper") {
         ++useWin;
-        return 'You Win! Scissors beats Paper';
+        return 'You Win! Scissor beats Paper';
      }
-     else if (playerSelection === "Paper" && computerSelection == "Paper") {
+     else if (playerSelection == "Paper" && computerSelection == "Paper") {
         return 'Tie! Play again!';
      }
      else if (playerSelection == "Paper" && computerSelection == "Rock") {
@@ -37,43 +72,29 @@ function playRound(playerSelection,computerSelection) {
         return 'You Win! Paper beats Rock';
      }
 
-     else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+     else if (playerSelection == "Scissor" && computerSelection == "Rock") {
         ++compWin;
-        return 'You Lose! Rock beats Scissors';
+        return 'You Lose! Rock beats Scissor';
      }
      else if (playerSelection == "Rock" && computerSelection == "Rock") {
         return 'Tie! Play again!';
      }
 
-     else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+     else if (playerSelection == "Paper" && computerSelection == "Scissor") {
         ++compWin;
-        return 'You Lose! Scissors beats Paper';
+        return 'You Lose! Scissor beats Paper';
     }
-    else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+    else if (playerSelection == "Scissor" && computerSelection == "Scissor") {
+        
         return 'Tie! Play again!';
     }
     else {
         ++useWin;
-        return 'You Win! Rock beats Scissors';
+        return 'You Win! Rock beats Scissor';
     }
 
 }
 
-function game() {
-    
-    for (let i = 0; i < 5; i++){
-     
-//      For game outcomes with out prompt remove line number 69
-        console.log(playRound(playerSelection,computerSelection))
-        console.log(computerSelection, userSelection);
-        userSelection = prompt("What is your choice? ")
-        computerSelection = getComputerChoice(choiceName);
-    }
-
-}
-game();
-
-console.log(useWin, compWin);
 
 
 
